6111d747494a5352eb528dba05ea219301d51b76
Liliana Gorga <gorgaliliana87@gmail.com>
2024-02-03 16:14:53 +0100

    remove CORS for production environment

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://techblog.lilianagorga.com', 'https://apitechblog.lilianagorga.com'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
